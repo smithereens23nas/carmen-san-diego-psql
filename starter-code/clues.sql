d2fe0ba7b3e8224b3c6ab2d0c76fa744d8835e94
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-02 18:04:15 -0500

    finished clue #7

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -62,3 +62,27 @@ FROM country
 WHERE code = 'BRA';
 --ANSWER: 211
 
+-- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
+ -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
+ -- landing dock.
+SELECT *
+FROM city
+WHERE id = 211;
+ 
+
+-- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
+-- if we can crack it, we can finally put her where she belongs – behind bars.
+
+-- Our play date of late has been unusually fun –
+-- As an agent, I'll say, you've been a joy to outrun.
+-- And while the food here is great, and the people – so nice!
+-- I need a little more sunshine with my slice of life.
+-- So I'm off to add one to the population I find
+-- In a city of ninety-one thousand and now, eighty five.
+
+
+-- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+
+
+-- She's in ______________________!
