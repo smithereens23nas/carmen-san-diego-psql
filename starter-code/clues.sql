9264e846cab77a22674104b7f269c7fef603dbe2
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-02 18:03:37 -0500

    Finished clue #6

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -54,3 +54,11 @@ WHERE city.name LIKE 'Serra%' AND country.continent = 'South America' AND countr
 --ANSWER: Brazil
 
 
+-- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
+ -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
+ -- follow right behind you!
+SELECT capital
+FROM country
+WHERE code = 'BRA';
+--ANSWER: 211
+
