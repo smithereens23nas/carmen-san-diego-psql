b930c01bce2595c1deb703102b49485db897fd5b
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-02 17:27:39 -0500

    Clues 1 through 4 are done

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -1,26 +1,48 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
+SELECT name, code, population
+FROM country
+WHERE region = 'Southern Europe'
+ORDER BY population
+LIMIT 1;
 
+--ANSWER: Vatican City State
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
+SELECT *
+FROM countrylanguage
+WHERE countrycode = 'VAT';
 
-
+--ANSWER: Italian
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
+SELECT countrycode
+FROM countrylanguage
+WHERE language = 'Italian' AND percentage = '100';
 
+SELECT name
+FROM country
+WHERE region = 'Southern Europe' AND code = 'SMR';
+
+--ANSWER: San Marino
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
+SELECT name
+FROM city
+WHERE countrycode = 'SMR' 
+AND name != 'San Marino'
 
+--ANSWER: Serravalle
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
